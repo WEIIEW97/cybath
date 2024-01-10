54bfc4e1dce4910a172836f01b237f20247ff1b9
william <williamwei9707@gmail.com>
2024-01-10 18:05:24 +0800

    add connected component algorithm.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include "src/connected_component.h"
+#include "src/estimate.h"
+
+using namespace std;
 
 int main(int, char**){
-    std::cout << "Hello, from landmark!\n";
+    string pgm_path = "/home/william/Codes/find-landmark/test.pgm";
+    auto pgm = cv::imread(pgm_path, -1);
+    auto exit_point = estimate_trajectory(pgm, 254, 15);
+    cv::Mat pgm_flip;
+    cv::flip(pgm, pgm_flip, 0);
+    for(auto& p : exit_point) {
+      cv::circle(pgm_flip, p, 2, cv::Scalar(125, 0, 0), 2);
+    }
+    cv::imshow("end point", pgm_flip);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+    return 0;
 }
---
+++ src/estimate.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022-2024, William Wei. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <numeric>
+#include "estimate.h"
+#include "connected_component.h"
+
+std::vector<cv::Point>
+row_searching_reduce_method(const cv::Mat& binary_mask) {
+  std::vector<cv::Point> middle_lane_coords;
+  int h = binary_mask.rows;
+
+  middle_lane_coords.reserve(h);
+
+#pragma omp parallel for
+  for (int i = 0; i < h; i++) {
+    auto row = binary_mask.row(i);
+    auto count = cv::countNonZero(row);
+
+    if (count > 0) {
+      cv::Mat locations;
+      cv::findNonZero(row, locations);
+      double sum = std::accumulate(
+          locations.begin<cv::Point>(), locations.end<cv::Point>(), 0.0,
+          [](double s, const cv::Point& p) { return s + p.x; });
+      double mean = sum / count;
+      middle_lane_coords.emplace_back(static_cast<int>(mean), i);
+    }
+  }
+  return middle_lane_coords;
+}
+
+std::vector<cv::Point> estimate_trajectory(const cv::Mat& pgm, int thr, int kernel_size) {
+  ConnectedComponent cc(pgm, thr);
+  cc.preprocess(kernel_size);
+  cv::Mat walkable = cc.find_largest_connected_component();
+  auto trajectory = row_searching_reduce_method(walkable);
+  return trajectory;
+}
