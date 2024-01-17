28ede3573e91d0cead4faea53f4869ad5166d47c
william <williamwei9707@gmail.com>
2024-01-17 18:23:52 +0800

    update code.

--- scratch/detect_start_line.py
+++ scratch/detect_start_line.py
@@ -1,8 +1,10 @@
 import numpy as np
 import cv2
+import os
 
-def get_rectangle_vertices(mask):
-    indices = np.where(mask == 255)
+
+def get_rectangle_vertices_simple(mask):
+    indices = np.where(mask >= 127)
     x_vec = indices[0]
     y_vec = indices[1]
 
@@ -18,21 +20,65 @@ def get_rectangle_vertices(mask):
     x_y_min = x_vec[np.where(y_vec == y_min)][0]
     x_y_max = x_vec[np.where(y_vec == y_max)][-1]
 
-    return [(x_min, x_min_y), (x_max, x_max_y), (x_y_min, y_min), (x_y_max, y_max)]   
+    return np.array([(x_min, x_min_y), (x_max, x_max_y), (x_y_min, y_min), (x_y_max, y_max)])
 
 
+def fit_rectangle(corners):
+    sorted_indices = np.lexsort((corners[:, 0], corners[:, 1]))
+    corners = corners[sorted_indices]
+    a, b, c, d = corners
 
+    print((a+b+c+d)[0]/4, (a+b+c+d)[1]/4)
+    virtual_c = np.array([(a+b+c+d)[0]/4, (a+b+c+d)[1]/4])
+    ab = b - a
+    bc = c - b
+    cd = d - c
+    da = a - d
+    
+    print(ab, bc, cd, da)
+
+    norm_ab = np.linalg.norm(ab)
+    norm_bc = np.linalg.norm(bc)
+    norm_cd = np.linalg.norm(cd)
+    norm_da = np.linalg.norm(da)
+
+    print(norm_ab, norm_bc, norm_cd, norm_da)
+
+    vec_norm = np.array([norm_ab, norm_bc, norm_cd, norm_da])
+    vec_side = np.array([ab, bc, cd, da])
+    # find the biggest vector, which might be the width side of the rectangle
+    idx = np.argmax(vec_norm)
+    w_side = vec_side[idx]
+    print(w_side)
+    w_side_p = np.array([-w_side[1], w_side[0]]) if w_side[1] >= 0 else np.array([w_side[1], -w_side[0]])
+
+    unit_w_side_p = w_side_p / np.linalg.norm(w_side_p)
+    return virtual_c, unit_w_side_p
 
 
 if __name__ == "__main__":
-    test_img = cv2.imread("/home/william/Codes/find-landmark/data/start_line/fake/start_0010.jpg", cv2.IMREAD_GRAYSCALE)
-    res = get_rectangle_vertices(test_img)
-    print(res)
-    img_copy = cv2.cvtColor(test_img, cv2.COLOR_GRAY2BGR)
-    for p in res:
-        x = p[0]
-        y = p[1]
-        cv2.circle(img_copy, (y, x), 1, (128, 127, 255), 1)
-    cv2.imshow("vertex", img_copy)
+    # test_img = cv2.imread(
+    #     "/home/william/Codes/find-landmark/data/start_line/fake/start_0001.jpg",
+    #     cv2.IMREAD_GRAYSCALE,
+    # )
+    basedir = "/home/william/Codes/find-landmark/data/start_line/fake"
+    all_file_names = [f for f in os.listdir(basedir)]
+    i = 1
+    for name in all_file_names:
+        test_img = cv2.imread(os.path.join(basedir, name), cv2.IMREAD_GRAYSCALE)
+        res = get_rectangle_vertices_simple(test_img)
+        # print(res)
+        c, unit_v = fit_rectangle(res)
+        end_point = unit_v * 200
+
+        img_copy = cv2.cvtColor(test_img, cv2.COLOR_GRAY2BGR)
+        for p in res:
+            x = p[0]
+            y = p[1]
+            cv2.circle(img_copy, (y, x), 1, (128, 127, 255), 1)
+        cv2.circle(img_copy, (int(c[1]), int(c[0])), 1, (0, 0, 255), 1)
+        cv2.line(img_copy, (int(c[1]), int(c[0])), (int(c[1] + end_point[1]), int(c[0] + end_point[0])), (128, 127, 255), 1)
+        cv2.imshow(f"vertex_{i}", img_copy)
+        i+=1
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- scratch/generate_fake_startline.m
+++ scratch/generate_fake_startline.m
@@ -3,7 +3,7 @@ function  generate_fake_startline()
 %   Detailed explanation goes here
 h = 10;
 w = 600;
-max_angle = 30;
+angle_range = [-15, 15];
 
 bg_h = 480;
 bg_w = 640;
@@ -13,9 +13,9 @@ scale = 1;
 savepath = "/home/william/Codes/find-landmark/data/start_line/fake";
 
 for i = 1:10
-    cx = randi(bg_w);
-    cy = randi(bg_h);
-    angle = randi(max_angle);
+    cx = randi([bg_w/2-30, bg_w/2+30]);
+    cy = randi([bg_h-60, bg_h-10]);
+    angle = randi(angle_range);
     mask = generate_fake_startline_impl(cx, cy, h, w, angle, bg_h, bg_w, scale);
     ext_name = 'start_%04d.jpg';
     full_name = sprintf(savepath+'/'+ext_name, i);
